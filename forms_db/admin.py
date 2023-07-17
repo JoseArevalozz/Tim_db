8e18b11f6ad327d657041e6916cd5793dae7cf06
JoseArevaloz7 <jose.arevaloz@fii-na.com>
2023-07-17 10:43:29 -0600

    created home page

--- forms_db/admin.py
+++ forms_db/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Employes
 
 # Register your models here.
+class FormEmployees(admin.ModelAdmin):
+    list_display = ("employee_number", "employee_name", "created")
+    search_fields = ("employee_number", "employee_name", "created")
+    list_filter = ("created", "employee_number")
+    ordering = ("employee_number",)
+
+
+admin.site.register(Employes, FormEmployees)
--- forms_db/models.py
+++ forms_db/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Employes(models.Model):
+    employee_number = models.CharField(max_length=10,primary_key=True)
+    employee_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=30, default='12345')
+    pmd = models.BooleanField(default=False)
+    dell = models.BooleanField(default=False)
+    switch = models.BooleanField(default=False)
+    mail = models.CharField(max_length=100, null=True)
+    privileges = models.CharField(max_length=10)
+    created = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+         return self.employee_number
