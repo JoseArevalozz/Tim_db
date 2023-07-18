0356ccf14be5560250488dec4050b81c5dc5134a
CrisCastellanosD <cristian.castellanos@fii-na.com>
2023-07-18 08:23:11 -0600

    uuts/admin

--- forms_db/admin.py
+++ forms_db/admin.py
@@ -8,6 +8,11 @@ class FormEmployees(admin.ModelAdmin):
     list_filter = ("created", "employee_number")
     ordering = ("employee_number",)
 
-
+class FormUuts(admin.ModelAdmin):
+    list_display = ("sn", "pn", "employee")
+    search_fields = ("sn", "pn", "employee")
+    list_filter = ("sn", "pn", "employee")
+    ordering = ("sn",)
+    
 admin.site.register(Employes, FormEmployees)
-admin.site.register(Uut)
+admin.site.register(Uut, FormUuts)
