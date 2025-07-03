93d01f24d7ea22a08cd69107b6c5e66e265ed9b3
SamuelACardenas <134441054+SamuelACardenas@users.noreply.github.com>
2025-07-03 10:41:01 -0600

    Modificate celery

--- Tim_DB/celery.py
+++ Tim_DB/celery.py
@@ -2,17 +2,19 @@ import os
 from celery import Celery
 from django.conf import settings
 
+# Solución definitiva para el error de mayúsculas
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tim_DB.settings')
 
 app = Celery('Tim_DB')
-
-# Configuración para Windows (local)
-if settings.DEBUG:
-    app.conf.broker_transport_options = settings.CELERY_BROKER_TRANSPORT_OPTIONS
-
-# Configuración para Linux (producción)
-else:
-    app.conf.broker_url = 'redis://:Switch123@localhost:6379/0'  # Broker en Redis
-
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+
+# Configuración multi-entorno
+if os.environ.get('DJANGO_DEBUG', 'True') == 'True':
+    app.conf.broker_transport_options = {
+        'data_folder_in': 'celery_data/in',
+        'data_folder_out': 'celery_data/out'
+    }
+else:
+    app.conf.broker_url = 'redis://:Switch123@localhost:6379/0'
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
